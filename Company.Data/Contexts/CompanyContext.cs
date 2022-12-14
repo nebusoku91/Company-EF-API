d2cb50a67880f18cb0c6514d78725d90f08ec56d
Jonathan Strand <jontepwn@gmail.com>
2022-12-14 15:53:30 +0100

    Add project files.

---
+++ Company.Data/Contexts/CompanyContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Company.Data.Contexts;
+
+internal class CompanyContext : DbContext
+{
+	public CompanyContext(DbContextOptions<CompanyContext> options) : base(options )
+	{
+
+	}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
